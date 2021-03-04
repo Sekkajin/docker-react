8af002b80982f578a332fe83817efee0dc8ab16c
Hugo KADI <hugo.kadi@acteam-it.com>
2021-03-04 14:45:50 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pénible allo ?
         </p>
         <a
           className="App-link"
